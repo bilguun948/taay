f2b2cff20000f37d4edcacc86c9c3fa3b0a73a8b
Bilguun unurdorj <bilguuntayland@gmail.com>
2025-11-16 00:31:22 +0800

    update

--- app/components/word.tsx
+++ app/components/word.tsx
@@ -16,13 +16,21 @@ export default function Word() {
   }
 
   function checkAnswer() {
-    setIsCorrect(currentWord.mongolian.some(answer => answer === userInput.trim()));
-  }
+  const normalizedInput = userInput.trim().toLowerCase().normalize("NFC");
+
+  const isMatch = currentWord.mongolian.some(answer => {
+    return answer.toLowerCase().normalize("NFC") === normalizedInput;
+  });
+
+  setIsCorrect(isMatch);
+}
 
   return (
     <div className="p-4">
-      <h1 className="">Герман үг: <b>{currentWord.german}</b></h1>
-
+        <div className="p-4 flex items-center justify-center bg-gray-900">
+      <h1 className="text-white">Герман үг: <b>{currentWord.german}</b></h1>
+      </div>
+    <div className="my-4 bg-gray-300 p-4">
       <input
         value={userInput}
         onChange={(e) => setUserInput(e.target.value)}
@@ -37,11 +45,13 @@ export default function Word() {
       <button onClick={getRandom} className="bg-gray-500 text-white p-2 ml-2">
         Дараагийн үг
       </button>
-
+</div>
+<div className="flex items-center justify-center">
       {isCorrect === true && <p className="text-green-500">Зөв!</p>}
       {isCorrect === false && (
         <p className="text-red-500">Буруу! Зөв хариулт: {currentWord.mongolian}</p>
       )}
+      </div>
     </div>
   );
 }
--- app/words.tsx
+++ app/words.tsx
@@ -4,8 +4,24 @@ export type Word = {
 };
 
 export const words: Word[] = [
-  { german: "anfangen", mongolian: ["эхлэх", "Эхлэх"] },
-  { german: "vergessen", mongolian: ["мартах", "Мартах"] },
-  { german: "erklären", mongolian: ["тайлбарлах"] },
-  { german: "Bescheid sagen", mongolian: ["мэдэгдэх"] },
+  { german: "anfangen", mongolian: ["Эхлэх"] },
+  { german: "arbeiten", mongolian: ["Ажиллах"] },
+  { german: "beginnen", mongolian: ["Эхлэх"] },
+  { german: "brauchen", mongolian: ["Хэрэгтэй"] },
+  { german: "denken", mongolian: ["Бодох"] },
+  { german: "dürfen", mongolian: ["Зөвшөөрөх"] },
+  { german: "entscheiden", mongolian: ["Шийдэх"] },
+  { german: "vergessen", mongolian: ["Мартах"] },
+  { german: "erklären", mongolian: ["Тайлбарлах"] },
+  { german: "Bescheid sagen", mongolian: ["Мэдэгдэх"] },
+  { german: "das Verhalten", mongolian: ["Зан байдал"] },
+  { german: "der Unterschied", mongolian: ["Ялгаа"] },
+  { german: "die Teilzeitarbeit", mongolian: ["Хагас цагийн ажил"] },
+  { german: "das gehalt", mongolian: ["Цалин"] },
+  { german: "die Verantwortung", mongolian: ["Хариуцлага"] },
+  { german: "das Gehalt", mongolian: ["Цалин"] },
+  { german: "die bewerbung", mongolian: ["Өргөдөл"] },
+  { german: "der Streit", mongolian: ["Маргаан"] },
+  { german: "das Klima", mongolian: ["Цаг агаар"] },
+  { german: "recyceln", mongolian: ["Дахин боловсруулах"] },
 ];
