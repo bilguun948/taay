893c4e9e95fc03bbb435f2920dc0b5e7ff70148e
Bilguun unurdorj <bilguuntayland@gmail.com>
2025-12-01 20:05:17 +0800

    update3

--- app/api/vortrag/[id]/route.ts
+++ app/api/vortrag/[id]/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest } from "next/server";
 import { pool } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: { id: string } }
 ) {
-  const id = Number(context.params.id);
+  const id = Number(params.id);
 
   const result = await pool.query(
     "SELECT * FROM vortrag WHERE id = $1",
