e48c50cbd8bb49119d50735c5ba175f23f2cd4b7
bilguun948 <bilguuntayland@gmail.com>
2026-01-09 04:38:28 +0800

    remove vortrag page

--- app/api/vortrag/route.ts
+++
@@ -1,6 +0,0 @@
-// import { pool } from "@/lib/db";
-
-// export async function GET() {
-//   const result = await pool.query("SELECT * FROM vortrag ORDER BY id ASC");
-//   return Response.json(result.rows);
-// }
